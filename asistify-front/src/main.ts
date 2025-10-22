82b4efd4052b2d9cd993dc7a8042aa5cde251198
Cucharoth <cucharoth@gmail.com>
2025-10-22 20:47:37 -0300

    feat(icons): added fontAwesome

--- asistify-front/src/main.ts
+++ asistify-front/src/main.ts
@@ -6,8 +6,22 @@ import './styles.css'
 import App from './App.vue'
 import router from './router'
 
+/* add fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 app.use(VueApexCharts);
