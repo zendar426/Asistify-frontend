ddddb21c87358c12dd72e9f214493473260601bb
Cucharoth <cucharoth@gmail.com>
2025-10-23 12:45:22 -0300

    feat(receptionist): completed repository CRUD

--- asistify-front/src/feature/receptionist/repository/ReceptionistRepositoryApi.ts
+++ asistify-front/src/feature/receptionist/repository/ReceptionistRepositoryApi.ts
@@ -8,6 +8,7 @@ import { ToastType } from '@/stores/ToastStore'
 import type { Result } from '@/utils/types'
 import { API_ROUTE } from '@/utils/config'
 import { useReceptionistStore } from '../stores/ReceptionistStore'
+import { tr } from 'zod/v4/locales'
 
 export class ReceptionistRepositoryApi implements ReceptionistRepository {
     private static instance: ReceptionistRepositoryApi
@@ -67,22 +68,130 @@ export class ReceptionistRepositoryApi implements ReceptionistRepository {
     }
 
     public async findById(id: string): Promise<Result<Receptionist | void>> {
-        // Implementation for fetching a receptionist by ID from API
-        return {} as Result<Receptionist>
+        try {
+            const response = await api.get(`${API_ROUTE}/receptionists/${id}`)
+
+            logger.info('[GET_RECEPTIONIST]', 'response: ', response)
+
+            if (response.status == 200) {
+                const receptionist = Receptionist.fromReceptionistDto(response.data)
+
+                return {
+                    success: true,
+                    message: 'Recepcionista obtenido con éxito',
+                    type: ToastType.success,
+                } as Result<Receptionist>
+            }
+
+            return {
+                success: false,
+                message: response.statusText,
+                type: ToastType.warning,
+            } as Result<Receptionist>
+        } catch (error: any) {
+            logger.error('[GET_RECEPTIONIST]', error)
+            return {
+                success: false,
+                message: error.message,
+                type: ToastType.error,
+            } as Result<Receptionist>
+        }
     }
 
     public async create(receptionist: Receptionist): Promise<Result<Receptionist | void>> {
-        // Implementation for creating a new receptionist via API
-        return {} as Result<Receptionist>
+        try {
+            const response = await api.post(`${API_ROUTE}/receptionists`, receptionist)
+
+            logger.info('[CREATE_RECEPTIONIST]', 'response: ', response)
+
+            if (response.status == 201) {
+                const createdReceptionist = Receptionist.fromReceptionistDto(response.data)
+
+                this.receptionistStore.addReceptionist(createdReceptionist)
+
+                return {
+                    success: true,
+                    message: 'Recepcionista creado con éxito',
+                    type: ToastType.success,
+                } as Result<Receptionist>
+            }
+
+            return {
+                success: false,
+                message: response.statusText,
+                type: ToastType.warning,
+            } as Result<Receptionist>
+        } catch (error: any) {
+            logger.error('[CREATE_RECEPTIONIST]', error)
+            return {
+                success: false,
+                message: error.message,
+                type: ToastType.error,
+            } as Result<Receptionist>
+        }
     }
 
     public async update(receptionist: Receptionist): Promise<Result<Receptionist | void>> {
-        // Implementation for updating a receptionist via API
-        return {} as Result<Receptionist>
+        try {
+            const response = await api.put(
+                `${API_ROUTE}/receptionists/${receptionist.id}`,
+                receptionist,
+            )
+            logger.info('[UPDATE_RECEPTIONIST]', 'response: ', response)
+
+            if (response.status == 200) {
+                const updatedReceptionist = Receptionist.fromReceptionistDto(response.data)
+                this.receptionistStore.updateReceptionist(updatedReceptionist)
+                return {
+                    success: true,
+                    message: 'Recepcionista actualizado con éxito',
+                    type: ToastType.success,
+                } as Result<Receptionist>
+            }
+
+            return {
+                success: false,
+                message: response.statusText,
+                type: ToastType.warning,
+            } as Result<Receptionist>
+        } catch (error: any) {
+            logger.error('[UPDATE_RECEPTIONIST]', error)
+            return {
+                success: false,
+                message: error.message,
+                type: ToastType.error,
+            } as Result<Receptionist>
+        }
     }
 
     public async delete(id: string): Promise<Result<Receptionist | void>> {
-        // Implementation for deleting a receptionist via API
-        return {} as Result<Receptionist | void>
+        try {
+            const response = await api.delete(`${API_ROUTE}/receptionists/${id}`)
+
+            logger.info('[DELETE_RECEPTIONIST]', 'response: ', response)
+
+            if (response.status == 200) {
+                this.receptionistStore.removeReceptionist(id)
+
+                return {
+                    success: true,
+                    message: 'Recepcionista eliminado con éxito',
+                    type: ToastType.success,
+                } as Result<Receptionist | void>
+            }
+
+            return {
+                success: false,
+                message: response.statusText,
+                type: ToastType.warning,
+            } as Result<Receptionist | void>
+        } catch (error: any) {
+            logger.error('[DELETE_RECEPTIONIST]', error)
+            return {
+                success: false,
+                message: error.message,
+                type: ToastType.error,
+            } as Result<Receptionist | void>
+        }
     }
 }
