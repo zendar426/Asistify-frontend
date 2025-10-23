e3c234f3310664adb469d6a1585ff966e57f7b54
Daniel <d.aguayo02@ufromail.cl>
2025-10-23 15:24:25 -0300

    usar form en modal de restablecer contraseña

--- asistify-front/src/feature/auth/components/PasswordModal.vue
+++ asistify-front/src/feature/auth/components/PasswordModal.vue
@@ -51,7 +51,8 @@ function clearEmail(){
                 </button>
             </div>
             <!-- Modal body -->
-            <div class="p-4 md:p-5 space-y-4">
+            <form @submit.prevent="">
+                <div class="p-4 md:p-5 space-y-4">
                 <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
                     Para reestablecer su contraseña introduzca el correo de la cuenta asociada. Le enviaremos un mensaje con los pasos a seguir
                 </p>
@@ -62,11 +63,12 @@ function clearEmail(){
                 
             </div>
             <!-- Modal footer -->
-            <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600 ">
+            <div class="flex items-center justify-end p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600 ">
                 
                 <button @click="clearEmail" data-modal-hide="pwModal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancelar</button>
-                <button @click="onRequestReset" data-modal-hide="pwModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Reestablecer</button>
+                <button @click="onRequestReset" data-modal-hide="pwModal" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Reestablecer</button>
             </div>
+            </form>
         </div>
     </div>
 </div>
