3127fa5f1dbb6ab3dd2db1b7c31245fd15e2747e
Daniel <d.aguayo02@ufromail.cl>
2025-10-24 12:45:23 -0300

    cambiar estilo de inputs detexto

--- asistify-front/src/feature/auth/components/LoginFormComponent.vue
+++ asistify-front/src/feature/auth/components/LoginFormComponent.vue
@@ -56,13 +56,13 @@ function login(){
         <div class="mb-5">
             <label for="email" class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo</label>
             <input type="email" id="email" v-model="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-primary focus:border-primary disabled:opacity-50 disabled:cursor-not-allowed"
                 placeholder="name@flowbite.com" required />
         </div>
         <div class="mb-5">
             <label for="password" class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
             <input type="password" id="password" v-model="password"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-primary focus:border-primary disabled:opacity-50 disabled:cursor-not-allowed"
                 required />
 
             
--- asistify-front/src/feature/receptionist/components/FormInput.vue
+++ asistify-front/src/feature/receptionist/components/FormInput.vue
@@ -17,6 +17,7 @@ const props = withDefaults(defineProps<Props>(), {
     type: 'text',
     placeholder: '',
     disabled: false,
+    isrequired:false,
 })
 
 const emit = defineEmits<{
