0e384fa250b5491507d1aeba053c1053f7ef451c
Zendar426 <m.espineira01@ufromail.cl>
2025-11-07 10:58:05 -0300

    feat(enterprise) remove an extra svg

--- asistify-front/src/feature/enterprise/views/EnterpriseView.vue
+++ asistify-front/src/feature/enterprise/views/EnterpriseView.vue
@@ -64,10 +64,6 @@ function handleSubmit() {
                             <option value="educacion">Educación</option>
                         </select>
                         <div class="absolute inset-y-0 right-0 flex items-center pr-4 pointer-events-none">
-                            <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M19 9l-7 7-7-7" />
-                            </svg>
                         </div>
                     </div>
                 </div>
