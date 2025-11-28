5a91791b5d3482b9475b188777415862528844ec
nicolas arias <n.arias10@ufromail.cl>
2025-11-28 00:08:33 -0300

    feat(auth): enhance user profile management and add authentication getters

--- asistify-front/src/stores/authStore.ts
+++ asistify-front/src/stores/authStore.ts
@@ -21,7 +21,7 @@ export interface UserProfile {
 export const useAuthStore = defineStore('auth', {
     state: () => ({
         accessToken: null as string | null,
-        userProfile: null,
+        userProfile: null as UserProfile | null,
         refreshToken: null as string | null,
     }),
     actions: {
@@ -48,15 +48,31 @@ export const useAuthStore = defineStore('auth', {
         },
         async loginUser(userData: LoginUserDto) {
             const response = await api.post('/auth/login', userData, {})
+            console.log(response)
             if (response.data.accessToken) {
                 this.setToken(response.data.accessToken)
             }
             if (response.data.refreshToken) {
                 this.setRefreshToken(response.data.refreshToken)
             }
+            if (response.data.user) {
+                this.userProfile = {
+                    email: response.data.user.email,
+                    name: response.data.user.name,
+                    phoneNumber: response.data.user.phoneNumber || null,
+                    avatar: response.data.user.avatar || null,
+                }
+            }
             console.log('Login response:', response)
             return response
         },
+    },
+    getters: {
+        isAuthenticated: (state) => !!state.accessToken,
+        userEmail: (state) => state.userProfile?.email || null,
+        userName: (state) => state.userProfile?.name || null,
+        userAvatar: (state) => state.userProfile?.avatar || null,
+        userPhoneNumber: (state) => state.userProfile?.phoneNumber || null,
     },
     persist: true,
 })
