39825537f0de833230e05f552de7dcf89ec59a8b
Cucharoth <cucharoth@gmail.com>
2025-12-04 01:17:50 -0300

    fix(ci): ensure network is specified when running the new container

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -39,6 +39,7 @@ echo "Starting new container on port $PORT..."
 docker run -d \
   --name asistify-frontend \
   --restart unless-stopped \
+  --network asistify-network \
   -p $PORT:8080 \
   $IMAGE_TAG
 
