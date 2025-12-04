330f047444f39bbe86a8650027d9f70859045354
Zendar426 <m.espineira01@ufromail.cl>
2025-12-03 22:12:51 -0300

    fix(axios): comment idle...

--- asistify-front/src/api/axios.ts
+++ asistify-front/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_API_URL,
+    baseURL: import.meta.env.VITE_BACKEND_API_URL, // fijarse en el .env y en el dockerfile
     // NO pongas el header Authorization aquí de forma estática
 })
 
