345d27db5add8e1517f421edd7f57ef9409b33f8
nicolas arias <n.arias10@ufromail.cl>
2025-12-04 21:13:50 -0300

    feature(membership): use Store to memberships and use local storage

--- asistify-front/src/api/membership.ts
+++ asistify-front/src/api/membership.ts
@@ -4,11 +4,13 @@ export interface IMembership {
     id: string
     name: string
     price: number
-    features: string[]
+    description: string
+    functionalities: string[]
 }
 export const getMembershipPlans = async (): Promise<IMembership[]> => {
     try {
         const response = await api.get('/membership')
+        console.log(response)
         return response.data
     } catch (error) {
         console.error('Error fetching membership plans:', error)
